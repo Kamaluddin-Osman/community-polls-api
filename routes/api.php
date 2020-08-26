1276362084dba8723c73016d729df2f691b3dfdf
Kamaluddin Osman <kamaluddin.osman@gmail.com>
2020-08-26 11:15:57 +0200

    frist push commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/polls' , 'PollsController@index');
+Route::get('/polls/{id}' , 'PollsController@show');
+Route::post('/poll', 'PollsController@store');
+Route::put('/poll/{poll}', 'PollsController@update');
+Route::delete('/poll/{poll}', 'PollsController@delete');
